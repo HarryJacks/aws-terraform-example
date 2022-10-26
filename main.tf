1052d2bea02497abac296e3ef6751dc868d441e1
Harry Jacks <harry@hjacksltd.co.uk>
2022-10-26 08:35:42 +0100

    Changed the proxy call so it specifically has a /hello endpoint in the gateway

--- main.tf
+++ main.tf
@@ -65,23 +65,23 @@ resource "aws_api_gateway_rest_api" "api_gw" {
     description = "API gateway v1"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "hello" {
     rest_api_id = aws_api_gateway_rest_api.api_gw.id
     parent_id = aws_api_gateway_rest_api.api_gw.root_resource_id
-    path_part   = "{proxy+}"
+    path_part   = "hello"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "hello" {
   rest_api_id = aws_api_gateway_rest_api.api_gw.id
-  resource_id = aws_api_gateway_resource.proxy.id
+  resource_id = aws_api_gateway_resource.hello.id
   http_method = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.api_gw.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_method.hello.resource_id
+  http_method = aws_api_gateway_method.hello.http_method
   integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = aws_lambda_function.hello.invoke_arn
